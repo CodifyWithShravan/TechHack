031c9536ff2e185558eba1e5fa7323c520ed9f7b
CodifyWithShravan <shravankumarthudi777@gmail.com>
2025-12-11 23:29:00 +0530

    fix tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
   },
-  plugins: [
-    require('@tailwindcss/typography'), // 👈 THIS IS THE MISSING PIECE
-  ],
-}
+})
